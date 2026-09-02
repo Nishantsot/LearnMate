f20689a3224dbf579beb2a1ceeb3a1cdd25c4251
Nishant Kumar <nishantkumar8357@gmail.com>
2026-09-02 10:58:53 +0530

    Fix Brevo email sender verification

--- src/main/java/learm/learn/Services/EmailService.java
+++ src/main/java/learm/learn/Services/EmailService.java
@@ -1,6 +1,6 @@
 package learm.learn.Services;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -8,14 +8,28 @@ import org.springframework.stereotype.Service;
 @Service
 public class EmailService {
 
-    @Autowired
-    private JavaMailSender mailSender;
+    private final JavaMailSender mailSender;
+
+    @Value("${app.mail.from}")
+    private String fromEmail;
+
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
 
     public void sendOtp(String to, String otp, String purpose) {
+
         SimpleMailMessage msg = new SimpleMailMessage();
+
+        msg.setFrom(fromEmail);
         msg.setTo(to);
         msg.setSubject("LearnMate - " + purpose);
-        msg.setText("Your OTP is: " + otp + "\nThis OTP will expire in 5 minutes.");
+
+        msg.setText(
+            "Your OTP is: " + otp +
+            "\n\nThis OTP will expire in 5 minutes."
+        );
+
         mailSender.send(msg);
     }
 }
